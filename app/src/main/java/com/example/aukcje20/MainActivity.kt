5e73330a81a0dc8c5840a1efa5e5047f053c3188
mtdph0en1x <4kproffesional@gmail.com>
2023-03-19 21:01:02 +0100

    menu

--- app/src/main/java/com/example/aukcje20/MainActivity.kt
+++ app/src/main/java/com/example/aukcje20/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.aukcje20
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("PrivateResource")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the DrawerLayout
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+
+        // Find the NavigationView
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        // Set up the ActionBar
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(com.google.android.material.R.drawable.ic_arrow_back_black_24)
+
+        // Set up the navigation drawer menu
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    // Handle Home
+                    true
+                }
+                R.id.nav_profile -> {
+                    // Handle Profile
+                    true
+                }
+                R.id.nav_settings -> {
+                    // Handle Settings
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Set up the drawer toggle
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        return if (drawerLayout.isDrawerOpen(findViewById(R.id.nav_view))) {
+            drawerLayout.closeDrawer(findViewById(R.id.nav_view))
+            true
+        } else {
+            super.onSupportNavigateUp()
+        }
     }
 }
